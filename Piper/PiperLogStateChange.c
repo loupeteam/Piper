8fd8de540b698dcc3eb02e5a27f8b279616313a5
sclaiborne <scott@loupe.team>
2020-02-19 17:43:01 -0800

    Add Piper

---
+++ Piper/PiperLogStateChange.c
@@ -0,0 +1,47 @@
+/********************************************************************
+ * COPYRIGHT --  
+ ********************************************************************
+ * Library: Piper
+ * File: PiperLogStateChange.c
+ * Author: Josh
+ * Created: October 08, 2013
+ ********************************************************************
+ * Implementation of library Piper
+ ********************************************************************/
+
+#include <bur/plctypes.h>
+#ifdef __cplusplus
+	extern "C"
+	{
+#endif
+
+	#include "Piper.h"
+
+#ifdef __cplusplus
+	};
+#endif
+
+/* Logs a state change. */
+plcbit PiperStateChange(struct Piper_typ* Piper, enum MACH_ST_enum State)
+{
+	BRSE_ARL_Arguments_typ LogData;
+	
+	STRING Oldstate[80];
+	STRING Newstate[80];	
+
+	//Get the strings to log
+	PackMLStateString(Piper->OUT.State,Oldstate);	
+	PackMLStateString(State,Newstate);	
+
+	LogData.s[0] = (UDINT)Oldstate;
+	LogData.s[1] = (UDINT)Newstate;
+	
+	//Log state change
+	logInfo(Piper->IN.CFG.LoggerName,0,"%s -> %s",(UDINT)&LogData);
+
+	//Change state
+	Piper->OUT.State = State;
+	Piper->OUT.SubState = 0;
+	
+	return 0;
+}
