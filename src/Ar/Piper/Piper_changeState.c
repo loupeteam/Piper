ef6375b2177928c2a748ae8418a7078df5c1d020
sclaiborne <scott@loupe.team>
2024-12-04 11:10:33 -0800

    Fix lost changes due to merge

--- src/Ar/Piper/Piper_changeState.c
+++ src/Ar/Piper/Piper_changeState.c
@@ -1,15 +1,3 @@
-<<<<<<<< HEAD:src/Ar/Piper/Piper_changeState.c
-/********************************************************************
- * COPYRIGHT --  
- ********************************************************************
- * Library: Piper
- * File: PiperLogStateChange.c
- * Author: Josh
- * Created: October 08, 2013
- ********************************************************************
- * Implementation of library Piper
- ********************************************************************/
-========
 /*
 * File: PiperLogStateChange.c
 * Copyright (c) 2023 Loupe
@@ -17,7 +5,6 @@
 * 
 * This file is part of the Piper Library, licensed under the MIT License.
 */
->>>>>>>> origin/main:src/Ar/Piper/PiperLogStateChange.c
 
 #include <bur/plctypes.h>
 #ifdef __cplusplus
@@ -31,37 +18,23 @@
 	};
 #endif
 
-<<<<<<<< HEAD:src/Ar/Piper/Piper_changeState.c
-// Performs and logs a state change
-plcbit Piper_changeState(struct Piper_typ* Piper, enum MACH_ST_enum State) {
-========
 /* Logs a state change. */
 plcbit PiperStateChange(struct Piper_typ* Piper, enum MACH_ST_enum State)
 {
->>>>>>>> origin/main:src/Ar/Piper/PiperLogStateChange.c
 	StrExtArgs_typ LogData;
 	
 	STRING Oldstate[80];
 	STRING Newstate[80];	
 
 	//Get the strings to log
-<<<<<<<< HEAD:src/Ar/Piper/Piper_changeState.c
 	PackMLStateString(Piper->OUT.State, Oldstate);	
 	PackMLStateString(State, Newstate);	
-========
-	PackMLStateString(Piper->OUT.State,Oldstate);	
-	PackMLStateString(State,Newstate);	
->>>>>>>> origin/main:src/Ar/Piper/PiperLogStateChange.c
 
 	LogData.s[0] = (UDINT)Oldstate;
 	LogData.s[1] = (UDINT)Newstate;
 	
 	//Log state change
-<<<<<<<< HEAD:src/Ar/Piper/Piper_changeState.c
 	logInfo(Piper->IN.CFG.LoggerName, 0, "%s -> %s", (UDINT)&LogData);
-========
-	logInfo(Piper->IN.CFG.LoggerName,0,"%s -> %s",(UDINT)&LogData);
->>>>>>>> origin/main:src/Ar/Piper/PiperLogStateChange.c
 
 	//Change state
 	Piper->OUT.State = State;
--- src/Ar/Piper/Piper_handleResponseState.c
+++ src/Ar/Piper/Piper_handleResponseState.c
@@ -18,63 +18,60 @@
 	};
 #endif
 
-/* This function looks at the response state and decides what Step or State to enter next */
-plcbit Piper_handleResponseState(struct Piper_typ* Piper)
-{
-
+// This function looks at the response state and decides what Step or State to enter next
+plcbit Piper_handleResponseState(struct Piper_typ* Piper) {
+	
 	struct Module_Interface_typ * Module;
 	StrExtArgs_typ LogData;
 	
-	//If we are done with the last step, check if there is another
+	// If we are done with the last step, check if there is another
 	switch (Piper->Internal.ResponseStatus)
 	{
 		case PIPER_RESPONSE_ST_STEP_DONE:
 			
-			//If the next step is greater than our current step, go to the next step	
-			if( Piper->OUT.SubState < Piper->Internal.NextSubState ){
+			// If the next step is greater than our current step, go to the next step	
+			if (Piper->OUT.SubState < Piper->Internal.NextSubState) {
 						
-				//Grab the next step
-				Piper->OUT.SubState= 		Piper->Internal.NextSubState;
-
-				//Next step is decided in checkResponses
-				Piper->Internal.NextSubState =	0;
-
-				//Set response status
+				// Grab the next step
+				Piper->OUT.SubState = Piper->Internal.NextSubState;
+				
+				// Next step is decided in checkResponses
+				Piper->Internal.NextSubState = 0;
+				
+				// Set response status
 				Piper->Internal.ResponseStatus = PIPER_RESPONSE_ST_NEXT_STEP;
 				
-				//Log the data if we can
-				if(Piper->OUT.SubStateRequestModule != 0){
+				// Log the data if we can
+				if (Piper->OUT.SubStateRequestModule != 0) {
 					Module = (Module_Interface_typ *)(Piper->OUT.SubStateRequestModule);
-					LogData.i[0] =  Piper->OUT.SubState;
-					LogData.s[0] =  (UDINT)&(Module->ModuleName);
-					logInfo(Piper->IN.CFG.LoggerName,0,"Substate %i requested by %s",(UDINT)&LogData);					
-				}			
+					LogData.i[0] = Piper->OUT.SubState;
+					LogData.s[0] = (UDINT)&Module->ModuleName;
+					logInfo(Piper->IN.CFG.LoggerName, 0, "Substate %i requested by %s", (UDINT)&LogData);
+				}
 			}
-			//If the next step is not greater than our current step, this state is done
-			else{
-
-				//We are done with this state
+			// If the next step is not greater than our current step, this state is done
+			else {
+				
+				// We are done with this state
 				Piper->Internal.ResponseStatus = PIPER_RESPONSE_ST_STATE_DONE;
-				Piper->OUT.SubState= 			IDLE_SUBSTATE;
-				Piper->Internal.NextSubState=	0;
-
-			}				
+				Piper->OUT.SubState = IDLE_SUBSTATE;
+				Piper->Internal.NextSubState = 0;
+			}
 			break;
 		
 		case PIPER_RESPONSE_ST_ERROR:
 			
 			Piper->IN.CMD.Abort = 1;
-
-			//Log the data if we can
-			if(Piper->OUT.ErrorModule != 0 ){
-				Module = (Module_Interface_typ *) Piper->OUT.ErrorModule;
-
-				if(Piper->OUT.State != MACH_ST_ABORTING ){
-					LogData.s[0] =  (UDINT)&(Module->ModuleName);
-					logInfo(Piper->IN.CFG.LoggerName,0,"%s requested an abort due to error",(UDINT)&LogData);				
-				}
 			
-			}			
+			// Log the data if we can
+			if (Piper->OUT.ErrorModule != 0) {
+				Module = (Module_Interface_typ *)Piper->OUT.ErrorModule;
+				
+				if (Piper->OUT.State != MACH_ST_ABORTING) {
+					LogData.s[0] = (UDINT)&Module->ModuleName;
+					logInfo(Piper->IN.CFG.LoggerName, 0, "%s requested an abort due to error", (UDINT)&LogData);
+				}
+			}
 			break;
 		
 		default:
@@ -82,5 +79,4 @@ plcbit Piper_handleResponseState(struct Piper_typ* Piper)
 	}
 	
 	return 1;
-	
 }
